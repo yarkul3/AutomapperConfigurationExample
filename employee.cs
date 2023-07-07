baff610ea52ff85d11fe10acb0c141bf4c1dc189
yarkul3 <133202905+yarkul3@users.noreply.github.com>
2023-07-07 09:32:51 +0200

    employee contracts

---
+++ employee.cs
@@ -0,0 +1,19 @@
+namespace AutoMapperDemo
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }
+	
+	public class EmployeeDTO
+    {
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+    }	
+}
+
